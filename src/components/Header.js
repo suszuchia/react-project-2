cc98839f3a42a9c5a7efd689db80e54260ee689e
Szuchia Su <suszuchia@gmail.com>
2024-07-21 17:12:28 +0100

    add logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
-import "./Header.css";
 
-const Header = () => {
+function Header() {
   return (
-    <header className="header">
-      <img src="logo.png" alt="Logo" className="logo" />
-      <h1 className="title">Currency Converter</h1>
+    <header>
+      <img src="/logo.png" alt="Logo" />
+      <h1>Currency Converter</h1>
     </header>
   );
-};
+}
 
 export default Header;
