8fd62ab9b2dda07e55cb8e99da61a03eb1556105
Szuchia Su <suszuchia@gmail.com>
2024-07-21 18:58:55 +0100

    add logo second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development", // Add this line
@@ -33,6 +34,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public", to: "", globOptions: { ignore: ["**/index.html"] } },
+      ],
     }),
   ],
   devServer: {
